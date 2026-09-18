e30ce682dc9726e02ab27529f97a47e3615286b0
German Caubarrere <germancaubarrere@gmail.com>
2026-09-18 12:56:20 -0300

    chore: add build and route infrastructure

---
+++ planificador-backend/src/config/constants.ts
@@ -0,0 +1,10 @@
+export const ALLOWED_MIME_TYPES = [
+  'image/jpeg',
+  'image/png',
+  'image/webp',
+  'application/pdf',
+];
+
+export const REFRESH_TOKEN_COOKIE = 'refresh_token';
+
+export const ALERT_THRESHOLD_DAYS = 15;
