2afcad496366308da6a79e3757457731c609bf21
German Caubarrere <germancaubarrere@gmail.com>
2026-09-18 12:55:30 -0300

    feat: publish core authenticated planning flow

---
+++ planificador-backend/src/types/express.d.ts
@@ -0,0 +1,12 @@
+import { Rol } from '@prisma/client';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        rol: Rol;
+      };
+    }
+  }
+}
